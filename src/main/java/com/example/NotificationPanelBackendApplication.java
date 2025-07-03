038fb9f8415fb4ebe82c39ba7cd759aa67a644de
Praneel Dev <devpraneel@gmail.com>
2025-07-03 23:40:51 +0530

    changed local timezone to IST

--- src/main/java/com/example/NotificationPanelBackendApplication.java
+++ src/main/java/com/example/NotificationPanelBackendApplication.java
@@ -3,6 +3,9 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class NotificationPanelBackendApplication {
     public static void main(String[] args) {
@@ -13,5 +16,11 @@ public class NotificationPanelBackendApplication {
         catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    @PostConstruct
+    public void init(){
+        // Setting Spring Boot default timezone to IST
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
     }
 }
