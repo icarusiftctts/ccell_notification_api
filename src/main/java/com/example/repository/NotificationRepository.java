3fabbebd95e12b2bf05295d47f5fd9a700afad00
Praneel Dev <devpraneel@gmail.com>
2025-07-11 23:08:16 +0530

    minor changes

--- src/main/java/com/example/repository/NotificationRepository.java
+++ src/main/java/com/example/repository/NotificationRepository.java
@@ -7,6 +7,5 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findByCreatedAtBefore(LocalDateTime time);
 }
 
