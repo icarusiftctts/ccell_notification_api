67b3aaa08bb5cf4b49b52ca716478b95a6fa1aab
Praneel Dev <devpraneel@gmail.com>
2025-07-14 22:37:32 +0530

    Added support for FCM

---
+++ src/main/java/com/example/service/FCMService.java
@@ -0,0 +1,46 @@
+package com.example.service;
+
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FCMService {
+
+    @Autowired
+    private FirebaseMessaging firebaseMessaging;
+
+    public void sendPush (String fcmToken, String title, String messageBody){
+        Message message = Message.builder()
+                .setToken(fcmToken)
+                .setNotification(Notification.builder()
+                        .setTitle(title)
+                        .setBody(messageBody)
+                        .build())
+                .build();
+        try {
+            String response = firebaseMessaging.send(message);
+            System.out.println("Notification sent: " + response);
+        } catch (Exception e){
+            System.out.println("Notification error: Not Sent, " + e.getMessage());
+        }
+    }
+
+    public void sendPushAll (String title, String messageBody){
+        Message message = Message.builder()
+                .setTopic("all-users")
+                .setNotification(Notification.builder()
+                        .setTitle(title)
+                        .setBody(messageBody)
+                        .build())
+                .build();
+        try {
+            String response = firebaseMessaging.send(message);
+            System.out.println("Notification sent: " + response);
+        } catch (Exception e){
+            System.out.println("Notification error: Not Sent, " + e.getMessage());
+        }
+    }
+}
