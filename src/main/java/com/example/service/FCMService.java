eee281ac10b47aef83d24f8745364c41dfd9ee70
Praneel Dev <devpraneel@gmail.com>
2025-07-21 19:39:51 +0530

    Added support for FCM - Notification Channel ID added

--- src/main/java/com/example/service/FCMService.java
+++ src/main/java/com/example/service/FCMService.java
@@ -1,8 +1,6 @@
     package com.example.service;
 
-    import com.google.firebase.messaging.Message;
-    import com.google.firebase.messaging.Notification;
-    import com.google.firebase.messaging.FirebaseMessaging;
+    import com.google.firebase.messaging.*;
     import org.springframework.beans.factory.annotation.Autowired;
     import org.springframework.stereotype.Service;
 
@@ -29,6 +27,13 @@
         }
 
         public void sendPushAll (String title, String messageBody){
+            AndroidConfig androidConfig = AndroidConfig.builder()
+                    .setPriority(AndroidConfig.Priority.HIGH)
+                    .setNotification(AndroidNotification.builder()
+                            .setChannelId("c-cell-notifs")
+                            .build())
+                    .build();
+
             Message message = Message.builder()
                     .setTopic("all-users")
                     .setNotification(Notification.builder()
